f3719df9db117834124a074f8060c41c27f04457
mazdok <grudzhyk777@gmail.com>
2019-10-23 15:42:20 +0300

    Add frontent skeleton

---
+++ frontend/src/pages/Auth.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class AuthPage extends Component {
+  render() {
+    return (
+      <h1>Auth</h1>
+    )
+  }
+}
+
+export default AuthPage
---
+++ frontend/src/pages/Bookings.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class BookingPage extends Component {
+  render() {
+    return (
+      <h1>Bookings page!</h1>
+    )
+  }
+}
+
+export default BookingPage
